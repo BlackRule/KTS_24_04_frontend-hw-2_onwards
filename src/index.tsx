4b25d4f6603dd79d9b1afd31440ebb929872596a
Your Name <you@example.com>
2024-04-21 22:45:20 +0200

    init

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,35 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './styles/styles.css';
-import Idx from "./Idx";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import {BrowserRouter, Navigate, Route, Routes} from 'react-router-dom'
+import {Header} from 'components/Header/Header'
+import Product from 'pages/Product'
+import Products from 'pages/Products'
 
 const root = ReactDOM.createRoot(
-    document.getElementById('root') as HTMLElement
-);
+  document.getElementById('root') as HTMLElement
+)
+
+const App=()=>{
+  return (
+    <BrowserRouter>
+      <div>
+        <Header/>
+        <Routes>
+          <Route path="/" element={<Products />} />
+          <Route path="/product">
+            <Route path=':id' element={<Product/>}/>
+            <Route path="" element={<Navigate to="/404" replace />} />
+          </Route>
+          <Route path="*" element={<Navigate to="/" replace />} />
+        </Routes>
+
+      </div>
+    </BrowserRouter>
+  )
+}
+
 root.render(
-    <React.StrictMode>
-      <Idx/>
-    </React.StrictMode>
-);
+  <React.StrictMode>
+    <App/>
+  </React.StrictMode>
+)
---
+++ src/pages/Products/components/PageNumbers/PageNumbers.tsx
@@ -0,0 +1,20 @@
+import classNames from 'classnames'
+import styles from './PageNumbers.module.scss'
+
+type PageNumbersProps= {
+    currentPage:number,
+    onChange:(currentPage: number) => void,
+    totalPages:number
+}
+
+const PageNumbers = ({currentPage,totalPages,onChange}:PageNumbersProps) => {
+  return (
+    <div className={styles.PageNumbers}>{[...Array(totalPages+1).keys()].slice(1).map(
+      (i)=>
+        <div key={i} onClick={()=>onChange(i-1)} className={
+          classNames(styles['page-number'],{[styles.selected]:i-1===currentPage})
+        }>{i}</div>
+    )}</div>
+  )
+}
+export default PageNumbers
